2a4594a673238a823b3b0953971d1917f5778978
alhaj <alhaj@alhajs-MacBook-Pro.local>
2024-02-24 23:30:06 +0600

    update

--- src/main/java/com/alhaj/servletexample/MyWebServlet.java
+++ src/main/java/com/alhaj/servletexample/MyWebServlet.java
@@ -6,11 +6,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class MyWebServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException
@@ -26,46 +21,4 @@ public class MyWebServlet extends HttpServlet {
         out.println("</body>");
         out.println("</html>");
     }
-
-   /* public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-//        response.setContentType("text/html");
-//        PrintWriter out = response.getWriter();
-
-        String n=request.getParameter("email");
-        String p=request.getParameter("password");
-
-        if(n.equals("alhaj@gmail.com") && p.equals("123456")){
-
-            System.out.println("Email "+n);
-            System.out.println("Pass "+p);
-
-            response.sendRedirect("welcome.jsp");
-
-
-        }else {
-
-            System.out.print("Wrong ");
-            response.sendRedirect("error.jsp");
-
-        }
-
-
-
-        // out.close();
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
-    }*/
 }
